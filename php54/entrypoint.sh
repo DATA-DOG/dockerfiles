ad12aabc4fdde0f7170f7d7e994349629adf9216
Vaidas Mikalauskas <vaidas.mikalauskas@gmail.com>
2015-09-14 14:09:06 +0300

    php 54 xdebug support

--- php54/entrypoint.sh
+++ php54/entrypoint.sh
@@ -5,5 +5,20 @@ if [ -f /init.sh ]; then
     sleep 5 && echo "[info] Running /init.sh script" && sh /init.sh &
 fi
 
-echo "Run: $@"
-exec "$@"
+if [ "$1" == "fpm" ]; then
+    if [ "$XDEBUG" != "" ]; then
+        PHP_ARGS="$PHP_ARGS -d xdebug.remote_enable=on"
+        if [ "$XDEBUG_HOST" == "" ]; then
+            XDEBUG_HOST="172.17.42.1"
+        fi
+    fi
+
+    if [ "$XDEBUG_HOST" != "" ]; then
+        PHP_ARGS="$PHP_ARGS -d xdebug.remote_host=$XDEBUG_HOST"
+    fi
+    echo "[info] starting php-fpm: /usr/sbin/php-fpm --nodaemonize $PHP_ARGS"
+    exec /usr/sbin/php-fpm --nodaemonize --force-stderr $PHP_ARGS
+else
+    echo "Run: $@"
+    exec "$@"
+fi
