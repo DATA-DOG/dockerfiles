ce1ef6db5e05ea541bf9ca8f5df12232ed8b2d2e
Vaidas Mikalauskas <vaidas.mikalauskas@gmail.com>
2015-10-28 09:01:41 +0200

    change php to ondemand

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-cp -f /etc/nginx/site-templates/*.conf /etc/nginx/sites-enabled/ &>> /dev/null
+for FILE in `ls -A /etc/nginx/site-templates/*.conf`; do
+    # reset template if it is not mounted
+    NAME=$(basename $FILE)
+    if mount | grep "/etc/nginx/sites-enabled/$NAME"; then
+        echo "[INFO] skiping template update: $FILE";
+    else
+        echo "[INFO] updating nginx site template:  /etc/nginx/sites-enabled/$NAME > $FILE";
+        cp -f "$FILE" "/etc/nginx/sites-enabled/$NAME"
+    fi
+done
 
 exec "$@"
