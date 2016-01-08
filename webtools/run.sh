c2d468942d7853570169aea38940c7b751310179
Vaidas Mikalauskas <vaidas.mikalauskas@gmail.com>
2016-01-08 10:12:13 +0200

    webtools

--- webtools/run.sh
+++ webtools/run.sh
@@ -14,22 +14,32 @@ WWW=`realpath "$WWW"`
 cd $WWW
 echo "Running in $WWW"
 
-if [ -f "$WWW/package.json" ]; then
-    echo "Running in npm install"
-    npm install
-fi
-
 if [ -f "$WWW/bower.json" ]; then
     echo "Running in bower"
     bower --allow-root install
 fi
 
-if [ -f "$WWW/gulpfile.js" ] && [ ! -z "$GULP_TASK" ]; then
-    echo "Running in gulp $GULP_TASK"
+if [ -f "$WWW/package.json" ]; then
+    echo "Running in npm install"
+    npm install
+fi
+
+if [ ! -z "$GULP_TASK" ]; then
+    echo "Running in gulp '$GULP_TASK"
     gulp $GULP_TASK
 fi
 
-if [ -f "$WWW/package.json" ] && [ ! -z "$NPM_TASK" ]; then
-    echo "Running in npm $NPM_TASK"
+if [ ! -z "$GRUNT_TASK" ]; then
+    echo "Running in grunt '$GRUNT_TASK"
+    grunt $GRUNT_TASK
+fi
+
+if [ ! -z "$NPM_TASK" ]; then
+    echo "Running npm '$NPM_TASK"
     npm $NPM_TASK
+fi
+
+if [ ! -z "$BUILD_TASK" ]; then
+    echo "Running build task '$BUILD_TASK'"
+    exec $BUILD_TASK
 fi
