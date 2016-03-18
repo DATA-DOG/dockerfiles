f7d62509e2da01ec6d82e0f642885614647487f0
Vaidas Mikalauskas <vaidas.m@datadog.org>
2016-03-18 15:07:00 +0200

    add php debug script

--- php70/entrypoint.sh
+++ php70/entrypoint.sh
@@ -8,7 +8,7 @@ if [ "$1" == "fpm" ]; then
     if [ "$XDEBUG" != "" ]; then
         PHP_ARGS="$PHP_ARGS -d xdebug.remote_enable=on"
         if [ "$XDEBUG_HOST" == "" ]; then
-            XDEBUG_HOST="172.17.42.1"
+            XDEBUG_HOST="172.17.0.1"
         fi
     fi
 
